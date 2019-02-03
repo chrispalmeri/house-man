7db49aa10ab0848e49a00bca86e0418d6b3d1667
Chris Palmeri <chrispalmeri@gmail.com>
2019-02-02 22:55:42 -0600

    Create database

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const sqlite3 = require('sqlite3');
+
+let db = new sqlite3.Database('database.db');
+db.run(`CREATE TABLE IF NOT EXISTS items (
+  item_id INT,
+  item_name TEXT,
+  item_quantity INT,
+  reorder_point INT,
+  reorder_store TEXT,
+  reorder_link TEXT,
+  order_date TEXT,
+  order_quantity INT,
+  order_count INT
+)`);
 
 let app = express();
 app.use(express.static('www'));
